b508fc69ca5b1fde0db86b6e6195a845f7e46f77
mehrthur-teste <mehrthurfordevtools@gmail.com>
2025-12-25 20:08:22 -0300

    Add Project one

---
+++ AzureOpenAIAgentApi/Dapper/EmployeeRepository.cs
@@ -0,0 +1,38 @@
+using Dapper;
+using Npgsql;  // Namespace do PostgreSQL
+using System.Data;
+using Microsoft.Data.Sqlite; // para SQLite
+using System.Threading.Tasks;
+
+public class EmployeeRepository
+{
+    private readonly string _connectionString;
+
+    public EmployeeRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    // Método para buscar todos os empregados
+    public async Task<IEnumerable<PersonEntity>> GetEmployeesAsync(string filter)
+    {
+        try
+        {
+            using (IDbConnection dbConnection = new NpgsqlConnection(_connectionString))
+            {
+                dbConnection.Open();
+                var employees = await dbConnection.QueryAsync<PersonEntity>(filter);
+                return employees;
+            } 
+        }
+        catch (Exception ex)
+        {
+            using (IDbConnection dbConnection = new SqliteConnection(_connectionString))
+            {
+                dbConnection.Open();
+                var employees = await dbConnection.QueryAsync<PersonEntity>(filter);
+                return employees;
+            }
+        }
+    }
+}
---
+++ AzureOpenAIAgentApi/Models/PersonInfo.cs
@@ -0,0 +1,8 @@
+public class PersonInfo
+{
+    public string? Name { get; set; }
+    public int? Age { get; set; }
+    public string? Occupation { get; set; }
+    public string? AIresponse { get; set; }
+}
+
